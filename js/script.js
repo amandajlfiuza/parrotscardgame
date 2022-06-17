07f92219d7b0473078b05adc61810bed07e57291
amandajlfiuza <amandafiuza.pj@gmail.com>
2022-06-16 22:12:03 -0300

    Cartas aleatórias

--- js/script.js
+++ js/script.js
@@ -1,31 +1,47 @@
+const baralho = [
+    "./imagens/bobrossparrot.gif"
+    ,
+    "./imagens/explodyparrot.gif"
+    ,
+    "./imagens/fiestaparrot.gif"
+    ,
+    "./imagens/metalparrot.gif"
+    ,
+    "./imagens/revertitparrot.gif"
+    ,
+    "./imagens/tripletsparrot.gif"
+    ,
+    "./imagens/unicornparrot.gif"
+]
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
 function virarCarta(elemento) {
     elemento.classList.toggle("virado");
 }
 
-function numeroCartas() {
+function criarCartas() {
     const noDeCartas = Number(prompt("Com quartas cartas você quer jogar? Insira um número par, de 4 a 14."));
     const numeroValido = (noDeCartas>=4 && noDeCartas<=14 && noDeCartas%2 == 0);
 
     if (numeroValido) {
-        alert("O número foi aceito");
-        const primeiraFileira = document.querySelector(".cards1");
-        const segundaFileira = document.querySelector(".cards2");
-        primeiraFileira.innerHTML = '';
-        segundaFileira.innerHTML = '';
-        for (let i=0; i<noDeCartas/2; i++) {
-            primeiraFileira.innerHTML += 
+        const cards = document.querySelector(".cards");
+        cards.innerHTML = '';
+
+        baralho.sort(comparador);
+
+        for (let i=0; i<noDeCartas; i++) {
+            cards.innerHTML +=
             `<div class='card' onclick='virarCarta(this)' data-identifier='card'>
                 <img class='frente' src='./imagens/front 1.png' data-identifier='back-face'/>
-                <img class='verso' src='./imagens/fiestaparrot.gif' data-identifier='front-face'/>
-            </div>`
-            segundaFileira.innerHTML += 
-            `<div class='card' onclick='virarCarta(this)' data-identifier='card'>
-                <img class='frente' src='./imagens/front 1.png' data-identifier='back-face'/>
-                <img class='verso' src='./imagens/fiestaparrot.gif' data-identifier='front-face'/>
-            </div>`
+                <img class='verso' src=${baralho[i]} data-identifier='front-face'/>
+            </div>`;
         }
+
     } else {
         prompt("Com quartas cartas você quer jogar? Insira um número par, de 4 a 14.")
     }
 }
-numeroCartas();
+criarCartas();
